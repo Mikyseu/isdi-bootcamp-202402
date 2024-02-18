0073fdbfe366e2a6f73ed0687bd8a93a6abcbdcf
Mikyseu <mikyseu@gmail.com>
2024-02-18 14:17:47 +0100

    WIP spaceship #163

--- staff/miquel-guevara/spaceinvaders/main.js
+++ staff/miquel-guevara/spaceinvaders/main.js
@@ -2,17 +2,19 @@ let ship = document.getElementById("ship");
 let alien = document.getElementsByClassName("alien");
 
 let x = 48.5;
-let y = 65;
+let y = 39;
 
 ship.style.left = x + "vw";
 ship.style.top = y + "vw";
 
 document.onkeydown = function (event) {
-  if (event.key === "ArrowLeft" && x > 31) x = x - 1;
-  else if (event.key === "ArrowRight" && x < 66) x = x + 1;
+  console.count("keydown")
+  
+  if (event.key === "ArrowLeft" && x > 0) x = x - 1;
+  else if (event.key === "ArrowRight" && x < 95) x = x + 1;
 
-  if (event.key === "ArrowUp" && y > 44) y = y - 1;
-  else if (event.key === "ArrowDown" && y < 65) y = y + 1;
+  if (event.key === "ArrowUp" && y > 0) y = y - 1;
+  else if (event.key === "ArrowDown" && y < 39) y = y + 1;
 
   ship.style.left = x + "vw";
   ship.style.top = y + "vw";
@@ -22,3 +24,7 @@ document.onkeydown = function (event) {
 
   console.log(shipRect, alienRect);
 };
+
+// if (shipInfo.left < alienInfo.right && shipInfo.right > alienInfo.left && shipInfo.top < alienInfo.bottom && shipInfo.bottom > alienInfo.top) {
+//   ship.src = 'images/explosion.png'
+// }
